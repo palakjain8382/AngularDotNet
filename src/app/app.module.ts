e107030cedc07f1dd944ca19c6b391c145135a2f
palakjain8382 <palakjain8382@gmail.com>
2023-01-24 16:16:54 +0530

    Read Operation

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
+import { AddEmployeeComponent } from './components/employees/add-employee/add-employee.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeesListComponent,
+    AddEmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
